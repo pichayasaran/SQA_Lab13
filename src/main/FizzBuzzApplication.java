1eb4d812c1fa901576de973ee76e51a9ad136575
pichayasaran <108724250+pichayasaran@users.noreply.github.com>
2022-10-11 21:45:15 +0700

    main3

--- src/main/FizzBuzzApplication.java
+++ src/main/FizzBuzzApplication.java
@@ -2,9 +2,12 @@ package main;
 
 public class FizzBuzzApplication {
 	 public String convert(int number){
-	        if(number == 2){ 
+		 if(number == 3) {
+	            return "Fizz";
+	     }
+	     if(number == 2){ 
 	            return "2";
-	        }
-	        return "1";
+	     }
+	     return "1";
 	 }
 }
