ef9e07550f711ea82b65e126b3856a6d5dd6add2
pichayasaran <108724250+pichayasaran@users.noreply.github.com>
2022-10-11 23:10:28 +0700

    mainfinished

--- src/main/FizzBuzzApplication.java
+++ src/main/FizzBuzzApplication.java
@@ -1,50 +1,17 @@
 package main;
 
 public class FizzBuzzApplication {
-	 public String convert(int number){
-		 if(number == 15){ 
-	            return "FizzBuzz";
-	     }
-		 if(number == 14){ 
-	            return "14";
-	     }
-		 if(number == 13){ 
-	            return "13";
-	     }
-		 if(number == 12){ 
-	            return "Fizz";
-	     }
-		 if(number == 11){ 
-	            return "11";
-	     }
-		 if(number == 10){ 
-	            return "Buzz";
-	     }
-		 if(number == 9){ 
-	            return "Fizz";
-	     }
-		 if(number == 8){ 
-	            return "8";
-	     }
-		 if(number == 7){ 
-	            return "7";
-	     }
-		 if(number == 6){ 
-	            return "Fizz";
-	     }
-		 if(number == 5){ 
-	            return "Buzz";
-	     }
-		 if(number == 4){ 
-	            return "4";
-	     }
-		 if(number == 3){ 
-	            return "Fizz";
-	     }
-	     if(number == 2){ 
-	            return "2";
-	     }
-	     return "1";
-	 }
+	public String convert(final int number) {
+		
+		if(canDivideBy(number, 3) && canDivideBy(number, 5)) return "FizzBuzz";
+		if(canDivideBy(number, 5)) return "Buzz";
+		if(canDivideBy(number, 3)) return "Fizz";
+
+		return String.valueOf(number);
+	}
+
+	private Boolean canDivideBy(int number, int devider){
+		return number % devider == 0;
+	}
 }
 
