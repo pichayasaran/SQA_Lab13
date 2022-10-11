0aeb6a50bde89ea5a5e9ce83e4eab3ce6b776c5a
panidamoknamtheng <panida.m@kkumail.com>
2022-10-11 22:08:13 +0700

    test5

---
+++ src/Test/Input5ShouldBeBuzz.java
@@ -0,0 +1,21 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import main.FizzBuzzApplication;
+
+class Input5ShouldBeBuzz {
+
+	@Test
+	public void Input2ShouldBe2() {
+	   String expected = "Buzz";
+	   int number = 5;
+
+	   FizzBuzzApplication fizzBuzz = new FizzBuzzApplication();
+	   String actual = fizzBuzz.convert(number);
+
+	   assertEquals(expected,actual);
+	}
+}
