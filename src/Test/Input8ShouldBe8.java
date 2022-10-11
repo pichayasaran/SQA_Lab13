1fcd3fff9d99e94ff938b6a5d904b1f0add3c04d
panidamoknamtheng <panida.m@kkumail.com>
2022-10-11 22:26:31 +0700

    test8

---
+++ src/Test/Input8ShouldBe8.java
@@ -0,0 +1,21 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import main.FizzBuzzApplication;
+
+class Input8ShouldBe8 {
+
+	@Test
+	public void Input2ShouldBe2() {
+	   String expected = "8";
+	   int number = 8;
+
+	   FizzBuzzApplication fizzBuzz = new FizzBuzzApplication();
+	   String actual = fizzBuzz.convert(number);
+
+	   assertEquals(expected,actual);
+	}
+}
