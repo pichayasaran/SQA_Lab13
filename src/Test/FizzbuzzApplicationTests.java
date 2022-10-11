402bc62af66c96dfb5ea62e9a02e4245056a3188
panidamoknamtheng <panida.m@kkumail.com>
2022-10-11 21:22:30 +0700

    update

---
+++ src/Test/FizzbuzzApplicationTests.java
@@ -0,0 +1,20 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FizzbuzzApplicationTests {
+
+	@Test
+	   public void Input1ShouldBe1() {
+
+	      String expected = "1";
+	      int number = 1;
+
+	      FizzBuzzApplication fizzBuzz = new FizzBuzzApplication();
+	      String actual = fizzBuzz.convert(number);
+
+	      assertEquals(expected,actual);
+	   }
+	}
