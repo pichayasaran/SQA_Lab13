3832321a81e4fe50f6fe55fb81ea6bae3b8f0770
panidamoknamtheng <panida.m@kkumail.com>
2022-10-11 21:34:30 +0700

    test2

---
+++ src/Test/Input2ShouldBe2.java
@@ -0,0 +1,19 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Input2ShouldBe2 {
+
+	@Test
+	public void Input2ShouldBe2() {
+	   String expected = "2";
+	   int number = 2;
+
+	   FizzBuzzApplication fizzBuzz = new FizzBuzzApplication();
+	   String actual = fizzBuzz.convert(number);
+
+	   assertEquals(expected,actual);
+	}
+}
